9dc30cd29d0e41869d0dae84a4ebcc5c7dcb2219
Paul Collins <paul.collins.iii@gmail.com>
2014-05-20 10:00:00 -0700

    Some more powerline setup.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,8 @@
 class powerline {
-  include repository
+  require python
 
   $powerline_repo = "${boxen::config::datadir}/powerline"
+  $fonts_installed_sentinal = '/var/db/.puppet_powerline_fonts_git'
 
   repository{
     $powerline_repo:
@@ -12,24 +13,46 @@ class powerline {
       provider => 'git';
   }
 
-  # Has to be installed to the global python location
-  exec { 'install powerline':
-    cwd     => "${powerline_repo}/powerline",
-    command  => ". ${boxen::config::home}/env.sh && \
-      pip install -e .",
-    provider => 'shell',
-    user     => $::boxen_user,
-    creates => "${homebrew::config::installdir}/lib/python2.7/site-packages/Powerline.egg-link";
+  file{
+    "/Users/${::boxen_user}/Library/Fonts":
+      ensure => directory;
+    "${boxen::config::envdir}/powerline.sh":
+      content => template('powerline/powerline.sh.erb');
   }
 
-  #TODO: Require python to be installed...
-  #TODO: Install the fonts for the system
-  # See https://github.com/robbiegill/puppet-font/blob/master/manifests/init.pp
-  #TODO: Setup a msg for the user to select a font to use in their terminal
+  # Has to be installed to the global python location
+  exec {
+    'install powerline':
+      cwd     => "${powerline_repo}/powerline",
+      command  => ". ${boxen::config::home}/env.sh && \
+        pip install -e .",
+      provider => 'shell',
+      user     => $::boxen_user,
+      require => Repository[$powerline_repo],
+      creates => "${homebrew::config::installdir}/lib/python2.7/site-packages/Powerline.egg-link";
+    'install powerline fonts to user':
+      cwd     => "${powerline_repo}-fonts",
+      path    => '/usr/bin',
+      command => "rsync -aPv */*.[ot]tf ~${::boxen_user}/Library/Fonts/",
+      require => [Repository["${powerline_repo}-fonts"], File["/Users/${::boxen_user}/Library/Fonts"]],
+      creates => $fonts_installed_sentinal,
+      notify  => [Exec["touch ${fonts_installed_sentinal}"],
+                  Notify['powerline-font notice']];
+    "touch ${fonts_installed_sentinal}":
+      command => "/usr/bin/touch ${fonts_installed_sentinal}";
+  }
+
+  notify{'powerline-font notice':
+    message => 'Powerline has been installed, but your fonts are going to be screwed up! You need to modify your \
+    terminal preferences to select one of the Powerline fonts, such as AnonymousPro'
+  }
+
   #TODO: Require janus, but what happens with vimrc.after?
   # convert to params? if so see file_line support from stdlib
+  # or use the puppet-concat module and let it add things in that way...
 
   #TODO: Add tmux set -g support?
   # convert to params? if so see file_line support from stdlib
+  # or use the puppet-concat module and let it add things in that way...
 
 }
